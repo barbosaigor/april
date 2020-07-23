ba4524d73f19dbaa02214ed04e17c0afcacfad91
Igor <igor.barbosa1920@gmail.com>
2020-07-23 14:56:33 -0300

    changed package description

--- doc.go
+++ doc.go
@@ -1,9 +1,7 @@
-// Package april implements chaos test tool.
+// Package april is a chaos testing tool.
 //
-//
-// April proposes improve resilience in microservices architectures. This
-// does chaos testing by randomly shutting down nodes, taking into account
-// their importance.
-// April is a CLI tool, being possible to either run chaos testing or others tools,
-// such as 'server' which hosts an API for remote access to the chaos test.
+// April proposes to improve resilience in microservices architectures. It
+// does chaos testing by randomly shutting down services, taking into account their importance.
+// April is similar to other tools, such as Chaos Monkey, Pumba, and Gremlin.
+// But it differs by the selection algorithm, which considers services importances (weights).
 package april
